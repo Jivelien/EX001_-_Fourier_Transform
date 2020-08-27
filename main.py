a2999d00dc1b26805ee36d92fb1fb86d05f37dca
Julien DELANNOY <julien.delannoy@hotmail.com>
2020-08-27 16:25:57 +0200

    harder than i though xD

--- main.py
+++ main.py
@@ -2,32 +2,26 @@ import numpy as np
 from math import pi, sqrt
 from matplotlib import pyplot as plt
 
-x = np.arange(0,5,1/1000)
-y = np.cos(x*2*pi*3)+1
-y=x
-plt.plot(x,y)
+def plotting(x,y):
+    ax = plt.gca()
+    ax.grid(True)
+    ax.spines['left'].set_position('zero')
+    ax.spines['bottom'].set_position('zero')
+    ax.plot(x, y)
+
+x = np.arange(0,30,1/1000)
+#y = np.cos(x*(2*pi)/3)
+y = np.cos(x)+1
+
 plt.polar(x,y)
+plt.plot(x,y)
+# x will define angle
+# y will define radius
 
-def cart2pol(x, y):
-    rho = np.sqrt(x**2 + y**2)
-    phi = np.arctan2(y, x)
-    return(rho, phi)
+# for training, value 2 will be a full circle
+#value_circle = 2
+#xx = np.sin(x*2*pi/value_circle )
+xx = np.sin(x)
+yy = np.cos(y)
 
-def pol2cart(rho, phi):
-    x = rho * np.cos(phi)
-    y = rho * np.sin(phi)
-    return(x, y)
-
-rho, phi = cart2pol(x,y)
-
-BAC = (1)*(pi/180)
-ABC = (90-BAC)*(pi/180)
-BCA = 90*(pi/180)
-
-AB = 1
-BC = np.sin(BAC)
-AC = np.cos(BAC)
-
-plt.scatter(AC,BC)
-plt.scatter(0,0)
-plt.scatter(1,1)
+plotting(xx, yy)
