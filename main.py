23107430b445a53a825afc8cb48fac3d106a5fb7
Julien DELANNOY <julien.delannoy@hotmail.com>
2020-08-27 17:15:31 +0200

    will explore with dash...

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ def plotting(x,y):
     ax.spines['bottom'].set_position('zero')
     ax.plot(x, y)
 
-x = np.arange(0,30,1/1000)
+x = np.arange(0,30,1/10)
 #y = np.cos(x*(2*pi)/3)
-y = np.cos(x)+1
+#y = np.cos(x)+1
+y = x*2
 
 plt.polar(x,y)
 plt.plot(x,y)
@@ -21,7 +22,7 @@ plt.plot(x,y)
 # for training, value 2 will be a full circle
 #value_circle = 2
 #xx = np.sin(x*2*pi/value_circle )
-xx = np.sin(x)
-yy = np.cos(y)
-
+length=100
+xx = np.sin(x[:length])
+yy = y[:length] * np.cos(xx[:length])
 plotting(xx, yy)
